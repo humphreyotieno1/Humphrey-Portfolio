722b467d5fd8143f3e7400e38fe9e98972c998fe
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-07-18 01:41:11 +0300

    Backend setup

--- server/app.py
+++ server/app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify
 from flask_mail import Mail, Message
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
