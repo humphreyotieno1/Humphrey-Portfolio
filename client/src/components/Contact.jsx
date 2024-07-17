8bd99fc29c2947eb235f2004ac9b4ce8fa21a621
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-07-18 01:59:07 +0300

    contact link

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/contact', {
+      const response = await fetch('https://humphrey-portfolio-rho.vercel.app/contact', {
         method: 'POST',
         body: formData,
       });
